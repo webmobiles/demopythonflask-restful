2acfa46c0aa83c5a3aa399b16342c41e85296668
Dave I <21120559+webmobiles@users.noreply.github.com>
2019-01-18 13:58:38 +0100

    Create testapi.sh

---
+++ testapi.sh
@@ -0,0 +1,1 @@
+curl -s -X POST --user "$MAILJET_API_KEY:$MAILJET_API_SECRET" https://api.mailjet.com/v3.1/send -H "Content-Type: application/json" -d @https://raw.githubusercontent.com/webmobiles/demopythonflask-restful/master/a.json
