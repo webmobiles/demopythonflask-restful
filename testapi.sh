e88c1a9ce840e9fd80acd6df4c954a041730530d
Dave I <21120559+webmobiles@users.noreply.github.com>
2019-01-18 14:09:03 +0100

    Update testapi.sh

--- testapi.sh
+++ testapi.sh
@@ -1,1 +1,2 @@
-curl -s -X POST --user "$MAILJET_API_KEY:$MAILJET_API_SECRET" https://api.mailjet.com/v3.1/send -H "Content-Type: application/json" -d @https://raw.githubusercontent.com/webmobiles/demopythonflask-restful/master/a.json
+echo  "api code: ${MAILJET_API_KEY}:${MAILJET_API_SECRET}"
+curl -s -X POST --user "${MAILJET_API_KEY}:${MAILJET_API_SECRET}" https://api.mailjet.com/v3.1/send -H "Content-Type: application/json" -d @a.json
